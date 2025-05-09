299a6bbe128b65785d5c937dcc74e72256e9f81e
SalvatoreFerro <71822226+SalvatoreFerro@users.noreply.github.com>
2025-05-09 20:31:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import GlyzeLanding from "./GlyzeLanding.js";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<GlyzeLanding />);

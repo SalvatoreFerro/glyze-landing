8cf461e1663b65c69f2ad3ce69c07d26648ec342
SalvatoreFerro <71822226+SalvatoreFerro@users.noreply.github.com>
2025-05-09 20:20:59 +0200

    Add files via upload

---
+++ GlyzeLanding.js
@@ -0,0 +1,9 @@
+// Placeholder, will be replaced later by the full code
+export default function GlyzeLanding() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Glyze - Menù NFC</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
---
+++ app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GlyzeLanding from "./GlyzeLanding.js";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<GlyzeLanding />);
